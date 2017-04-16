6310a802713b91ca7bd6ceb506ad59d8236920cb
Paweł Preczyński <pawel.preczynski@gmail.com>
2017-04-16 21:29:27 +0200

    Updated main screen functional test.

--- tests/functional/main.js
+++ tests/functional/main.js
@@ -7,12 +7,6 @@ module.exports = {
       .assert.title('Cordova + Webpack')
       .assert.visible('.received')
       .assert.hidden('.listening')
-      .end();
-  },
-
-  'Should present status on center of the screen': function (browser) {
-    browser
-      .url('http://localhost:8080/')
       .resizeWindow(400, 600)
       .pause(1000)
       .saveScreenshot('tmp/tests/screenshot/main.png')
